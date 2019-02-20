ec1933c2f628a70a0424218c3f2bfaee5a82a1bf
Vitaliy <vitaliy.igorevich@mail.ru>
2019-02-20 18:35:17 +0300

    add test search

--- test_algorithms.py
+++ test_algorithms.py
@@ -35,3 +35,22 @@ def test_count_sort():
                              [5]*10 + [3]*5 + [9]*9 + [1]*3 + [5]*3,
                              [1, 3, 5, 9]) == ([1]*3 + [3]*5 + [5]*13
                                                + [9]*9)
+
+
+def test_search():
+    functions_search = (
+        algs.liner_search,
+        algs.binary_search,
+    )
+
+    for function in functions_search:
+        assert function(1, [1, 2, 3, 4, 5]) == 0
+        assert function(5, [1, 2, 3, 4, 5]) == 4
+        assert function(0, [1, 2, 3, 4, 5]) is None
+        assert function(6, [1, 2, 3, 4, 5]) is None
+        assert function(10, [1, 2, 3, 4, 5]) is None
+        assert function(-1, [1, 2, 3, 4, 5]) is None
+        assert function(1, []) is None
+        assert function(1, [1]) == 0
+        assert function(1, [1, 2]) == 0
+        assert function(2, [1, 2]) == 1
